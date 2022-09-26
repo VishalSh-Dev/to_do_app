38bf0e2bedeb5c6c675e628c22734caad7665887
VishalSh-Dev <vishalshaw1658@gmail.com>
2022-09-26 21:43:13 +0530

    first commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:to_do_app/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List toDoList = [
+    ["Make Coffee", false],
+    ["Do code", true]
+  ];
+
+  void checkbox(bool value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text('TO DO'),
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkbox,
+          );
+        },
+      ),
+    );
+  }
+}
